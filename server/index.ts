6159bb6718ddabebf5576ffd1f35f690c2e2df97
otaku.denka <otaku.denka@gmail.com>
2019-05-27 15:59:46 +0800

    feat(all files): use tslint

--- server/index.ts
+++ server/index.ts
@@ -6,23 +6,30 @@ const dev = process.env.NODE_ENV !== 'production';
 const app = Next({ dev });
 const handle = app.getRequestHandler();
 
-app.prepare().then(() => {
-  const server = new Koa();
-  const router = new Router();
+app.prepare().then(
+  (): void => {
+    const server = new Koa();
+    const router = new Router();
 
-  server.use(router.routes());
+    server.use(router.routes());
 
-  server.use(async (ctx: Koa.BaseContext) => {
-    await handle(ctx.req, ctx.res);
-    ctx.respond = false;
-  });
+    server.use(
+      async (ctx: Koa.BaseContext): void => {
+        await handle(ctx.req, ctx.res);
+        ctx.respond = false;
+      },
+    );
 
-  server.use(async (ctx: Koa.BaseContext, next: Function) => {
-    ctx.res.statusCode = 200;
-    await next();
-  });
+    server.use(async (ctx: Koa.BaseContext, next: void) => {
+      ctx.res.statusCode = 200;
+      await next();
+    });
 
-  server.listen(3000, () => {
-    console.log('koa server listening on 3000');
-  });
-});
+    server.listen(
+      3000,
+      (): void => {
+        console.log('koa server listening on 3000');
+      },
+    );
+  },
+);
